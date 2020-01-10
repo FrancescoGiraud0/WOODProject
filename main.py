4c6d8fbb9f8724a4d3761605578c8e800afacdff
NikoWar <43783505+NikoWar@users.noreply.github.com>
2020-01-10 15:23:47 +0000

    main.py + rawFunction

--- main.py
+++ main.py
@@ -9,13 +9,53 @@ from time import sleep
 import random
 import os
 
+import cv2 as cv
+import numpy as np 
+
 import shutil
 
-src = "sample_data/"
+src = "sample_data/zz_astropi_1_photo_"
 dst = "capture/"
-imageName = "zz_astropi_1_photo_"
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def avg_center(percentage, path):
+    '''
+    This function returns  a numpy array containing the average  bgr value of a certain percentage of pixels starting from the center of an image.
+    '''
+    
+    rgb_list = []
+
+    img = cv.imread(path)
+    height,width,_ = img.shape
+
+    centerX = (width // 2 ) 
+    centerY = (height // 2)                                                                  
+                                                            # XLB,YTB --->    #####  <---- XRB,YTB
+                                                                                #   #
+                                                                                #   #
+                                                            # XLB,YBB ---->   #####  <---- XRB,YBB
+
+    #RightBorder 
+    XRB = centerX + ((width * percentage)//200)                    
+    #LeftBorder
+    XLB = centerX - ((width * percentage)//200)
+    #TopBorder
+    YTB = centerY + ((height * percentage)//200)
+    #BottomBorder
+    YBB = centerY - ((height * percentage)//200)
+
+    #print(f"{height}, {width} xrb  {XRB} ---- xlb {XLB} ----- ytb {YTB} ----- ybb {YBB}") 
+
+    for x in range(XLB,XRB):
+        for y in range(YBB,YTB):
+            rgb_list.append(img[y,x])
+            #print((img[y,x]))
+
+    numpy_bgr_array = np.array(rgb_list)
+    average_value = np.average(numpy_bgr_array,axis=0)
+    #print(f"valore mediio -- {average_value.astype(int)}")
+    return average_value.astype(int)
 
 # Connect to the Sense Hat
 #sh = SenseHat()
@@ -85,12 +125,13 @@ while (now_time < start_time + datetime.timedelta(minutes=178)):
         #cam.captureDumpy(dir_path+"/photo_"+ str(photo_counter).zfill(3)+".jpg")
         
         #COPYING PHOTO FROM INITIAL DIRECTORY TO DESTINATION
-        if cnt < 455:
-            shutil.copy2((src+ imageName + cnt), dst)
+        if cnt < 455 and avg_center(10, (src + cnt)):
+            shutil.copy2((src + cnt), dst)
             photo_counter+=1
             cnt += 1
         else:
             break
+        
         # update the current time
         now_time = datetime.datetime.now()
     except Exception as e:
