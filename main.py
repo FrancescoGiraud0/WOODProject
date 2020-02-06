53af5101f1c0345de930b5839592a13a7e3fd81c
NikoWar <43783505+NikoWar@users.noreply.github.com>
2020-02-06 15:24:26 +0100

    add commits

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from time import sleep
 import os
 import cv2 as cv
 import numpy as np
+from sklearn.cluster import KMeans
 # -------------------------------
 
 # Setup
@@ -43,6 +44,8 @@ iss  = ephem.readtle(name, l1, l2)
 CAM_RESOLUTION = (2592,1952) #(2592,1944)
 CAM_FRAMERATE  = 32
 DIFF_THRESHOLD = 0.3
+PIXEL_THRESHOLD = 6
+ML_MIN_N_OF_SAMPLES = 5
 
 # Set up camera
 cam = PiCamera()
@@ -65,7 +68,7 @@ def get_latlon():
     and returns it.
     '''
 
-    iss.compute() # Get the lat/long values from ephem
+    iss.compute() # Get the lat/lon values from ephem
     long_value = [float(i) for i in str(iss.sublong).split(":")]
 
     if long_value[0] < 0:
@@ -113,19 +116,76 @@ def is_day(img, size_percentage=10, min_threshold=85):
 
     for x in range(XLB,XRB):
         for y in range(YBB,YTB):
-            bgr_list.append(img[y,x])
+            bgr_list.append(img[y,x])   #this for take the value the value in bgr of every pixel in the pic
 
-    numpy_bgr_array = np.array(bgr_list)
-    average_value = np.average(numpy_bgr_array,axis=0)
+    numpy_bgr_array = np.array(bgr_list)    #convert bgr_list in a numpy array
+    average_value = np.average(numpy_bgr_array,axis=0)  #calculate the average value of blue, green and red
 
-    average_value = average_value.astype(int)
+    average_value = average_value.astype(int)   #convert the type of datas
 
-    average_value = np.uint8([[[average_value[0],average_value[1],average_value[2]]]])
+    average_value = np.uint8([[[average_value[0],average_value[1],average_value[2]]]])  #map the values from 0 to 255  
 
-    gray_avg_values = cv.cvtColor(average_value,cv.COLOR_BGR2GRAY)
-    gray_avg_values = np.squeeze(gray_avg_values)
+    gray_avg_values = cv.cvtColor(average_value, cv.COLOR_BGR2GRAY)  #convert the color from bgr to grayscale
+    gray_avg_values = np.squeeze(gray_avg_values)   #remove single-dimensional entries from the shape of an array
 
     return gray_avg_values >= min_threshold
+
+def calculateStatisticFast(matrix, pixel_threshold=PIXEL_THRESHOLD, diff_threshold=DIFF_THRESHOLD):
+    NDVIGraduation = {
+        0 : 0, # <0.1
+        1 : 0, # 0.1-0.2
+        2 : 0, # 0.2-0.3
+        3 : 0, # 0.3-0.4
+        4 : 0, # 0.4-0.5
+        5 : 0, # 0.5-0.6
+        6 : 0, # 0.6-0.7
+        7 : 0, # 0.7-0.8
+        8 : 0, # 0.8-0.9
+        9 : 0, # 0.9-1.00
+        'diff' : 0 # Number of pixel with constrast (Forest-Desert, Forest-Cities)
+    }
+    shape = np.shape(matrix)
+    temp = matrix/255.0
+    nofpixel = 1.0 * shape[0]*shape[1]
+    
+    for i,val in enumerate(np.histogram(temp[1:-1,1:-1], bins=[0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])[0]):
+        NDVIGraduation[i] = val/nofpixel
+     
+    diff_10 = np.where((temp - np.roll(temp,shift=1,axis=0)) > DIFF_THRESHOLD,1,0)
+    diff_11 = np.where((temp - np.roll(temp,shift=1,axis=1)) > DIFF_THRESHOLD,1,0)
+    diff_matrix_thr = np.where(temp>PIXEL_THRESHOLD/10.,diff_10+diff_11,0)
+    
+    NDVIGraduation['diff'] = (diff_matrix_thr[1:-1,1:-1].sum())/nofpixel
+
+    return NDVIGraduation
+
+def contrast_stretch(im):
+    """
+    Performs a simple contrast stretch of the given image, from 5-95%.
+    """
+    in_min = np.percentile(im, 5)
+    in_max = np.percentile(im, 100)
+
+    out_min = 0.0
+    out_max = 255.0
+
+    out = im - in_min
+    out *= ((out_min - out_max) / (in_min - in_max))
+    out += in_min
+
+    return out
+ 
+def calculateNDVI(image):
+    b, _, r = cv.split(image) #extract bgr values 
+    bottom = (r.astype(float) + b.astype(float))   
+    bottom[bottom == 0] = 0.0000000000001 # Make sure to not divide by zero
+
+    ndvi = (r.astype(float) - b) / bottom
+    ndvi = contrast_stretch(ndvi)
+    ndvi = ndvi.astype(np.uint8)
+    
+    return ndvi
+
 
 def read_sh_data(take_pic, photo_id):
     if not take_pic:
@@ -140,7 +200,7 @@ def read_sh_data(take_pic, photo_id):
     lat, lon = get_latlon()
 
     # Save the data to the log file
-    data_logger.info("%s , %s , %s , %s , %s", photo_id, lat, lon, mag_x, mag_y, mag_z)
+    data_logger.info("%s, %s , %s , %s , %s , %s", photo_id, lat, lon, mag_x, mag_y, mag_z)
 #--------------------------------
 
 def run():
@@ -152,16 +212,15 @@ def run():
     
     # Counter to store the number of saved photos
     photo_counter = 1
-
     info_logger.info('Starting the experiment')
-
+    X_data = np.empty((0,11), float) #!!!!!!!!! 11 is the number of keys of NDVIGraduation!!!!!!!!!
     # This will loop for 3 hours
-    while (now_time < start_time + datetime.timedelta(minutes=5)):
+    while (now_time < start_time + datetime.timedelta(minutes=180)):
         try:
             # Take a pic 
             cam.capture(rawCapture, format="bgr")
             image = rawCapture.array
-            
+        
             take_pic = is_day(image)
 
             info_logger.debug("Take pic: %s", take_pic)
@@ -170,14 +229,31 @@ def run():
             read_sh_data(take_pic, photo_counter)
 
             if take_pic:
+                NDVI_image = calculateNDVI(image) #NDVI image calc (0-255 range)
+                NDVI_stats = calculateStatisticFast(NDVI_image)
+                
+                data_logger.info("%s, %s", photo_counter, ','.join([str(v) for v in NDVI_stats.values()]))  #store datas in .csv file
+
                 # Use zfill to pad the integer value used in filename to 3 digits (e.g. 001, 002...)
                 file_name = dir_path + "/img_" + str(photo_counter).zfill(3) + ".jpg"
 
                 # Saving the images
                 cv.imwrite(file_name, image)
 
-                info_logger.info('Photo %s saved', photo_counter )
+                info_logger.info('Photo %s saved', photo_counter)
 
+                #ML clustering algo
+                curr_sample = np.array([[v for v in NDVI_stats.values()]])
+                X_data = np.append(X_data, curr_sample, axis=0)
+                if len(X_data) > ML_MIN_N_OF_SAMPLES:
+                     try:
+                        info_logger.info("KMEANS is running")
+                        kmeans = KMeans(n_clusters=4, random_state=0).fit(X_data) #Clustering KMEANS algo
+                        curr_label = kmeans.predict(curr_sample) #Predicting image label
+                        data_logger.info("%s, Cluster_%s", photo_counter, str(curr_label[0]))
+                     except Exception as e_algo:
+                        info_logger.error("An algorithm error occurred: " + str(e_algo))
+                    
                 # Sleep for 5 seconds
                 sleep(5)
 
@@ -195,3 +271,4 @@ def run():
         info_logger.info('End of the experiment')
 
 run()
+
