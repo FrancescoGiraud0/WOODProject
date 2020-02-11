e251519a997a6937a06f596a5a26c1442325c70c
MatteoAllemandi <46619805+MatteoAllemandi@users.noreply.github.com>
2020-02-11 17:40:25 +0100

    Add files via upload

---
+++ functions/printOnScreen.py
@@ -0,0 +1,12 @@
+def writeOnimage(img,):
+    font= cv.FONT_HERSHEY_SIMPLEX       #text font
+    bottomLeftCornerOfText=(0,200)      #bottom Left Corner of the text
+    fontScale=0.3                       #text scale
+    fontColor=(255, 0, 0)               #textColor
+    lineType= 1                         #line size
+    text = 'Hello World!'               #text To Wrirte
+
+    cv.putText(img,text,bottomLeftCornerOfText,font,fontScale,fontColor,lineType)
+
+    #Display the image
+    #cv2.imshow("img",img)
